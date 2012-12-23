b0e2a119936d2650f4222516e1122a8d27444b7f
victor <vicapow@gmail.com>
2012-12-23 17:20:35 -0500

    changed get to post

--- routes/github-hook.js
+++ routes/github-hook.js
@@ -2,7 +2,7 @@ var fs = require('fs')
   , md = require("node-markdown").Markdown
 
 module.exports = function(app){
-  app.get('/github-hook', function(req, res, next){
+  app.post('/github-hook', function(req, res, next){
     console.log('request for github-hook: ' + req.connection.remoteAddress)
     res.send(200)
   })
