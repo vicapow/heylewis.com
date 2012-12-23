5cb2ebf63bf88ac2b22dacef8f09808f846b0aef
victor <vicapow@gmail.com>
2012-12-23 17:24:36 -0500

    restrict to github

--- routes/github-hook.js
+++ routes/github-hook.js
@@ -5,6 +5,10 @@ module.exports = function(app){
   app.post('/github-hook', function(req, res, next){
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress
     console.log('request for github-hook: ' + ip)
-    res.send(200)
+    if( ip !== '207.97.227.253'
+      && ip !== '50.57.128.197'
+      && ip !== '108.171.174.178'
+    ) return res.send(404) // the request didn't come from github
+    console.log(req.body)
   })
 }
